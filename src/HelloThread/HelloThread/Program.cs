19e3bd0d98f39b2817fd526d3da9a7a5699c63e5
karenten10 <karenten10@gmail.com>
2020-09-20 19:50:54 +0800

    first commit

---
+++ src/HelloThread/HelloThread/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===> 1 " + DateTime.Now);
+            Method1();
+        }
+
+        static void Method1()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("===> 2 " + DateTime.Now);
+        }
+    }
+}
