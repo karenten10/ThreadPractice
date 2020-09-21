0215f8a66a01b441a0907a43254e2672a189f5cf
Karen <karenten10@gmail.com>
2020-09-21 08:57:44 +0800

    add example in office

---
+++ src/asyncHello/asyncHello/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asyncHello
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            int foo;
+            foo = 168;
+
+            int bar = await MyMethod();
+            string bar2 = await MyMethodStr();
+
+            foo = foo + bar;
+
+            Console.WriteLine($"====> foo =>{foo}");
+        }
+
+        static Task<int> MyMethod()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 99;
+            });
+        }
+
+        static Task<string> MyMethodStr()
+        {
+            return Task.FromResult("");
+        }
+
+        
+    }
+}
